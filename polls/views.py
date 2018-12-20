08dcc0eebc951eb9b3e6970dea1c70ca7ac3f415
bt699 <32189166@qq.com>
2018-12-20 15:10:01 +0800

    views results

--- polls/views.py
+++ polls/views.py
@@ -33,9 +33,12 @@ def detail(request,question_id):
 
 
 
+# def results(request,question_id):
+#     response = "you're looking at the results of question %s."
+#     return  HttpResponse(response % question_id)
 def results(request,question_id):
-    response = "you're looking at the results of question %s."
-    return  HttpResponse(response % question_id)
+    question = get_object_or_404(Question,pk=question_id)
+    return render(request,'polls/results.html',{'question':question})
 
 # def vote(request,question_id):
 #     return HttpResponse("you're voting on question %s." % question_id)
