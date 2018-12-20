bd732d792689dd4c3b3160b788fcbc8283d59921
bt699 <32189166@qq.com>
2018-12-20 16:07:44 +0800

    异常报错’ ‘

--- polls/views.py
+++ polls/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render,get_object_or_404
+from django.shortcuts import get_object_or_404,render
 from django.http import  HttpResponse,Http404,HttpResponseRedirect
 from .models import Question
 from django.urls import reverse
