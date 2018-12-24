f5ed8538ba4c897092512d972d4e30b3ae32fc1a
bt699 <32189166@qq.com>
2018-12-24 15:30:23 +0800

    多三个选项添加单元

--- polls/admin.py
+++ polls/admin.py
@@ -3,13 +3,23 @@ from .models import Question,Choice
 
 # class QuestionAdmin(admin.ModelAdmin):
 # # #     fields = ['pub_date','question_text']
+# class QuestionAdmin(admin.ModelAdmin):
+#     fieldsets = [
+#         (None, {'fields':['question_text']}),
+#         ('Date information',{'fields':['pub_date']}),
+#     ]
+class ChoiceInline(admin.StackedInline):
+    model = Choice
+    extra = 3
+
 class QuestionAdmin(admin.ModelAdmin):
     fieldsets = [
-        (None, {'fields':['question_text']}),
-        ('Date information',{'fields':['pub_date']}),
+        (None,{'fields':['question_text']}),
+        ('Date information',{'fields':['pub_date'],'classes':['collapse']}),
     ]
+    inlines = [ChoiceInline]
 admin.site.register(Question,QuestionAdmin)
-admin.site.register(Choice)
+# admin.site.register(Choice)
 
 
 # Register your models here.
