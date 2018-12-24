4d5436bedeee752461d59c60e358023c3c68a417
bt699 <32189166@qq.com>
2018-12-24 13:49:02 +0800

    test_future_question

--- polls/tests.py
+++ polls/tests.py
@@ -40,3 +40,11 @@ class QuestionIndexViewTests(TestCase):
             response.context['latest_question_list'],
             ['<Question:Past question.>']
         )
+
+    def test_future_question(self):
+        create_question(question_text="Future question.",days=30)
+        response = self.client.get(reverse('polls:index'))
+        self.assertContains(response,"No polls are available.")
+        self.assertQuerysetEqual(response,context['latest_question_list'],[])
+
+    def test_
