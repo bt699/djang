31924a997a6c654ec0b99b00ab6311afb6f20928
bt699 <32189166@qq.com>
2018-12-24 14:02:50 +0800

    测试DetailView

--- polls/views.py
+++ polls/views.py
@@ -31,6 +31,8 @@ class IndexView(generic.ListView):
 class DetailView(generic.DetailView):
     model = Question
     template_name = 'polls/detail.html'
+    def get_queryset(self):
+        return Question.objects.filter(pub_date__lte=timezone.now())
 
 # def detail(request,question_id):
 #     question = get_object_or_404(Question,pk=question_id)
