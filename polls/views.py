7255ce8b6065160b03291d1cbc47a5203b78ae42
bt699 <32189166@qq.com>
2018-12-24 12:27:47 +0800

    改设置 timezone=utc

--- polls/views.py
+++ polls/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import get_object_or_404,render
 from django.http import  HttpResponseRedirect
 from django.urls import reverse
+from django.utils import timezone
 from django.views import generic
 from .models import Choice, Question
 # def index(request):
@@ -20,7 +21,7 @@ from .models import Choice, Question
 
 class IndexView(generic.ListView):
     template_name = 'polls/index.html'
-    context_object_name = 'latest_qestion_list'
+    context_object_name = 'latest_question_list'
     def get_queryset(self):
         return Question.objects.order_by('-pub_date')[:5]
 
