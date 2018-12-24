bf1d427b000198b1bbfc4e76fb2619bcafd787c7
bt699 <32189166@qq.com>
2018-12-24 14:20:02 +0800

    test_pass_question

--- polls/tests.py
+++ polls/tests.py
@@ -71,4 +71,9 @@ class QuestionDetalViewTests(TestCase):
         url = reverse('polls:detail',args=(future_question.id,))
         response = self.client.get(url)
         self.assertEqual(response.status_code,404)
-        
+
+    def test_pass_question(self):
+        past_question = create_question(question_text='Past Question',days=-5)
+        url = reverse('polls:detail',args=(past_question.id,))
+        response = self.client.get(url)
+        self.assertContains(response,past_question.question_text)
